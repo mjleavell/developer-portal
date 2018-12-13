f31d580695e0413cb44b96d51e350be83a22feaa
Maggie Leavell <mjleavell@gmail.com>
2018-12-12 18:22:55 -0600

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    console.log(this);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button className='btn btn-danger'>HELP ME</button>
+          <Button
+              tag="a"
+              color="success"
+              size="large"
+              href="http://reactstrap.github.io"
+              target="_blank"
+          >
+              View Reactstrap Docs
+          </Button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
