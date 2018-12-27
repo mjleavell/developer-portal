9d31adec105e2359a30dc088317179f5c67e446b
Maggie Leavell <mjleavell@gmail.com>
2018-12-27 17:41:31 -0600

    tutorials displaying

--- src/components/Portal/Portal.js
+++ src/components/Portal/Portal.js
@@ -2,14 +2,25 @@ import React from 'react';
 import './Portal.scss';
 import { Card } from 'reactstrap';
 import PortalNav from '../PortalNav/PortalNav';
+import TutorialItem from '../TutorialItem/TutorialItem';
 
 class Portal extends React.Component {
   render() {
+    const { tutorials } = this.props;
+
+    const tutorialItemComponents = tutorials.map(tutorial => (
+      <TutorialItem
+        tutorial={tutorial}
+        key={tutorial.id}
+      />
+    ));
+
     return (
       <div className="portal">
         <Card body outline color="secondary" className="p-0">
           <PortalNav />
-          <div className="portal-output">
+          <div className="portal-output p-1">
+            {tutorialItemComponents}
           </div>
         </Card>
       </div>
